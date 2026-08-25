1f678d120da599fc439f6987650d5cab12533980
romaina229 <nounonvenanromain@gmail.com>
2026-08-25 23:54:26 +0100

    valide icon

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1280,
     height: 800,
+    icon: path.join(__dirname, 'build', 'icon.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
